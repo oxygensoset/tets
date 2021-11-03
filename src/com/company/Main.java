fb05b42f4fba2b0fc9f8edf21f3a491fbc6b8dde
oxygensoset <saveliy.burmistrov.99@mail.ru>
2021-11-03 14:57:55 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double cathetus1, cathetus2, hypot;
+    cathetus1 = 3; // длина первого катета
+      cathetus2 = 4; // длина второго катета
+   hypot = Math.sqrt((cathetus1 * cathetus1)+
+                      (cathetus2 * cathetus2));
+    }
+}
